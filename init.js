1ba165414df0be916667ab6986c01cd22b7af8ca
Andrey Strelin <andrejmax@yandex.ru>
2021-04-13 12:34:28 +0400

    update styles, resolve point 4

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-
+/*
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
@@ -9,5 +9,27 @@ window.onload = function()
     document.getElementById('patronymicOutput').innerText = initPerson.patronymic;
     document.getElementById('professionOutput').innerText = initPerson.profession;
 
-};
+};*/
+
+document.getElementById('generate_button').addEventListener('click', () => {
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
+    document.getElementById('genderOutput').innerText = initPerson.gender + ',';
+    document.getElementById('birthYearOutput').innerText = initPerson.birthDay + ',';
+    document.getElementById('patronymicOutput').innerText = initPerson.patronymic;
+    document.getElementById('professionOutput').innerText = initPerson.profession;
+
+});
+
+document.getElementById('clear_button').addEventListener('click', () => {
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = '';
+    document.getElementById('surnameOutput').innerText = '';
+    document.getElementById('genderOutput').innerText = '';
+    document.getElementById('birthYearOutput').innerText = '';
+    document.getElementById('patronymicOutput').innerText = '';
+    document.getElementById('professionOutput').innerText = '';
+
+});
 
